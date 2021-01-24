3dbf539effb30ad76f90008b4c5f0167173214da
jonathan-dwight <jd.buendia0711@gmail.com>
2021-01-24 10:08:29 -0800

    Day 3 - Picture in Picture

---
+++ picture-in-picture/script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+
+const selectMediaStream = async () => {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        console.log('select media stream error:', error)
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable button
+    button.disabled = true;
+    // Start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button;
+    button.disabled = false;
+    
+});
+
+selectMediaStream();
